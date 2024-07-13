13eb5c6413d16aa0e5f531da6204f96df5c188e9
tuzov <roma_tuzov2016@mail.ru>
2024-07-13 19:59:41 +0300

    - add profile

--- app/src/main/java/com/example/databasexmlcourse/features/feature_profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/databasexmlcourse/features/feature_profile/ProfileViewModel.kt
@@ -3,30 +3,30 @@ package com.example.databasexmlcourse.features.feature_profile
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.databasexmlcourse.core.BaseViewModel
+import com.example.databasexmlcourse.core.models.AlertData
 import com.example.databasexmlcourse.domain.domain_api.UsersUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
-    private val usersUseCase: UsersUseCase
+    // UseCase
 ) : BaseViewModel<ProfileViewModel.State, ProfileViewModel.Actions>(State()) {
 
+    fun onExitButtonClick() {
+        onAction(Actions.ShowAlert)
+    }
+
+    fun onConfirmExitClick() {
+        // TODO - Exit
+    }
 
     data class State(
-        var title: String = ""
+        val name: String = ""
     )
 
     sealed interface Actions {
-        data class ShowAlert(val alertData: String) : Actions
+        data object ShowAlert : Actions
     }
 
-    class ProfileViewModelFactory @Inject constructor(
-        private val usersUseCase: UsersUseCase
-    ) : ViewModelProvider.Factory {
-
-        override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return ProfileViewModel(usersUseCase) as T
-        }
-    }
 }
